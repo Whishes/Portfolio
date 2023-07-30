1a740de54237644ded75d981bd8a34d7405fd469
Nicholas <nicholas.cruickshank25@gmail.com>
2023-07-30 20:19:35 +1000

    fixing random fav issue

--- src/Skills.tsx
+++ src/Skills.tsx
@@ -2,18 +2,33 @@ import db from "./db.json";
 import Skill from "./Skill";
 
 const Skills = () => {
-	return (
-		<section className="content-section darkmode" id="skills-section">
-			<h2 className="heading" aria-label="Skills">
-				My Skills
-			</h2>
-			<ul className="links">
-				{db.skills.map((skill) => {
-					return <Skill key={skill.id} skillName={skill.name} />;
-				})}
-			</ul>
-		</section>
-	);
+  interface TestObject {
+    name: string;
+    number: number;
+  }
+  const exObject: Object = {
+    name: "20",
+    number: 10,
+  };
+
+  function test(object: TestObject, id: string) {
+    const { name, number } = object;
+  }
+
+  console.log(test("Sam", "Ken"));
+
+  return (
+    <section className="content-section darkmode" id="skills-section">
+      <h2 className="heading" aria-label="Skills">
+        My Skills
+      </h2>
+      <ul className="links">
+        {db.skills.map((skill) => {
+          return <Skill key={skill.id} skillName={skill.name} />;
+        })}
+      </ul>
+    </section>
+  );
 };
 
 export default Skills;
