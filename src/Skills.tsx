906545963a39e907af0ce9106063beff005674c5
Nicholas <nicholas.cruickshank25@gmail.com>
2023-07-30 20:20:32 +1000

    revert changes made in class to demonstrate TS

--- src/Skills.tsx
+++ src/Skills.tsx
@@ -2,21 +2,6 @@ import db from "./db.json";
 import Skill from "./Skill";
 
 const Skills = () => {
-  interface TestObject {
-    name: string;
-    number: number;
-  }
-  const exObject: Object = {
-    name: "20",
-    number: 10,
-  };
-
-  function test(object: TestObject, id: string) {
-    const { name, number } = object;
-  }
-
-  console.log(test("Sam", "Ken"));
-
   return (
     <section className="content-section darkmode" id="skills-section">
       <h2 className="heading" aria-label="Skills">
