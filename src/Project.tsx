3cd6073a5d240479212830c9d97c04853a36164b
Nicholas <nicholas.cruickshank25@gmail.com>
2023-08-03 11:00:40 +1000

    removing lists for now after render & heroku changes until I find a good way to host all the posgresSQL instances

--- src/Project.tsx
+++ src/Project.tsx
@@ -6,63 +6,53 @@ import project_3 from "./images/project_3.png";
 import project_4 from "./images/project_4.png";
 
 const Project = ({
-	pid,
-	name,
-	demo_link,
-	github_link,
-	img_link,
-	description,
-	languages,
+  pid,
+  name,
+  demo_link,
+  github_link,
+  img_link,
+  description,
+  languages,
 }: {
-	pid: number;
-	name: string;
-	demo_link: string;
-	github_link: string;
-	img_link: string;
-	description: string;
-	languages: string;
+  pid: number;
+  name: string;
+  demo_link: string;
+  github_link: string;
+  img_link: string;
+  description: string;
+  languages: string;
 }) => {
-	const getImport = (id: number) => {
-		switch (id) {
-			case 1:
-				return project_1;
-			case 2:
-				return project_2;
-			case 3:
-				return project_3;
-			case 4:
-				return project_4;
-		}
-	};
-
-	return (
-		<div className="individual-project">
-			<h3 className="project-heading">{name}</h3>
-			<a href={demo_link}>
-				<img
-					className="project-img"
-					src={getImport(pid)}
-					alt={name + " screenshot"}
-				/>
-			</a>
-			<h4>{languages}</h4>
-			<p className="project-paragraph">{description}</p>
-			<ul className="links">
-				<li className="li-item">
-					<a className="project-link" href={demo_link}>
-						<CgWebsite />
-						Project Demo
-					</a>
-				</li>
-				<li className="li-item">
-					<a className="project-link" href={github_link}>
-						<BsGithub />
-						Github
-					</a>
-				</li>
-			</ul>
-		</div>
-	);
+  return (
+    <div className="individual-project">
+      <h3 className="project-heading">{name}</h3>
+      <a href={demo_link}>
+        <img
+          className="project-img"
+          src={`images/project_${pid}.png`}
+          alt={name + " screenshot"}
+        />
+      </a>
+      <h4>{languages}</h4>
+      <p className="project-paragraph">{description}</p>
+      <ul className="links">
+        {/* render and heroku is incredibly expensive for more than 1 instance of posgresSQL */}
+        {pid === 1 && (
+          <li className="li-item">
+            <a className="project-link" href={demo_link}>
+              <CgWebsite />
+              Project Demo
+            </a>
+          </li>
+        )}
+        <li className="li-item">
+          <a className="project-link" href={github_link}>
+            <BsGithub />
+            Github
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Project;
